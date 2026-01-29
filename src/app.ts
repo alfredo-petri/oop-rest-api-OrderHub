0c6c5af8bbf82def5a48cad5fe716558439323fa
alfredo_petri <alfredo_petri@hotmail.com>
2026-01-29 08:15:31 -0300

    feat: add helmet for enhanced security

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import cors from 'cors'
 import { errorHandler } from './middlewares/error-handler'
 import { routes } from './routes'
 import dotenv from 'dotenv'
+import helmet from 'helmet'
 
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
+
+app.use(helmet())
 
 app.use(express.json())
 
